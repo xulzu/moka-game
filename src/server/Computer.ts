c6ad8c87170f165096c10a5ca9346f3e0bde58a0
xufujun <xulzu@qq.com>
2025-08-06 10:15:57 +0800

    s

--- src/server/Computer.ts
+++ src/server/Computer.ts
@@ -45,39 +45,120 @@ export class Computer {
       }
     }
   }
+  private async playOneCard(id: number) {
+    if (id === -1) return;
+    await new Promise<void>((res) => {
+      setTimeout(() => {
+        res();
+      }, 2000);
+    });
+    this.zoom?.playCard(this.zoomRole, id);
+    await new Promise<void>((res) => {
+      if (this.self.danger === -1) {
+        res();
+      } else {
+        this.waitFinish = res;
+      }
+    });
+  }
   //打攻击牌
   async randomPlayAttck() {
-    const getRandomAttack = () => {
-      const attackIds = this.self.handCards
-        .filter((item) => item.type === "attack")
-        .map((item) => item.id);
-      if (attackIds.length === 0) {
-        return -1;
-      }
-      const idx = Math.floor(Math.random() * attackIds.length);
-      return attackIds[idx];
-    };
-
-    const playOneAttackCard = async () => {
-      const id = getRandomAttack();
-      if (id === -1) return;
-      await new Promise<void>((res) => {
-        setTimeout(() => {
-          res();
-        }, 2000);
-      });
-      this.zoom?.playCard(this.zoomRole, id);
-      await new Promise<void>((res) => {
-        if (this.self.danger === -1) {
-          res();
+    const cards = this.self.handCards.filter((item) => item.type === "attack");
+    const cardsSpecial = this.self.handCards.filter(
+      (item) => item.type === "special"
+    );
+    let play3attack = false;
+    {
+      //先打策略牌，最优先打过牌
+      for (const card of cardsSpecial) {
+        if (card.name === "防火墙升级") {
+          const cardsDefense = this.self.handCards.filter(
+            (item) => item.type === "defense"
+          );
+          if (cardsDefense.length) {
+            await this.playOneCard(card.id);
+          }
         } else {
-          this.waitFinish = res;
+          await this.playOneCard(card.id);
+          play3attack = card.name === "攻击脚本优化";
         }
-      });
-    };
-    //尝试打出两张攻击牌
-    await playOneAttackCard();
-    await playOneAttackCard();
+      }
+    }
+
+    let id1 = -1;
+    let id2 = -1;
+    let id3 = -1;
+    (() => {
+      if (id1 !== -1) return;
+      //判断手上是否同时有web攻击和提权攻击
+      const web = cards.find(
+        (item) => item.tag2 === "WEB_ATTACK" && item.name !== "提权攻击"
+      );
+      const tq = cards.find((item) => item.name === "提权攻击");
+      if (web && tq) {
+        id1 = web.id;
+        id2 = tq.id;
+      }
+    })();
+
+    (() => {
+      if (id1 !== -1) return;
+      //判断手上是否有两张弱口令攻击
+      const c1 = cards.find((item) => item.name === "弱口令攻击");
+      const c2 = cards.find(
+        (item) => item.name === "弱口令攻击" && item.id !== c1.id
+      );
+
+      if (c1 && c2) {
+        id1 = c1.id;
+        id2 = c2.id;
+      }
+    })();
+
+    (() => {
+      if (id1 !== -1) return;
+      //判断手上是否有社工和恶意广告
+      const c1 = cards.find(
+        (item) => item.tag2 === "SOCIAL_ENGINEERING" && item.name !== "恶意广告"
+      );
+      const c2 = cards.find((item) => item.name === "恶意广告");
+      if (c1 && c2) {
+        id1 = c1.id;
+        id2 = c2.id;
+      }
+    })();
+
+    (() => {
+      if (id1 !== -1) return;
+      //是否能先打攻击小的再打大的
+      const cardsOrder = [...cards].sort((a, b) => a.attack - b.attack);
+      const c1 = cardsOrder[0];
+      const c2 = cardsOrder[1];
+      if (c1 && c2) {
+        id1 = c1.id;
+        id2 = c2.id;
+      }
+    })();
+
+    if (id1 === -1) {
+      id1 = cards[0]?.id ?? -1;
+    }
+    if (id2 === -1) {
+      id2 = cards[1]?.id ?? -1;
+    }
+    if (play3attack) {
+      const c3 = cards.find((item) => item.id !== id1 && item.id !== id2);
+      id3 = c3?.id;
+    }
+
+    id1 ??= -1;
+    id2 ??= -1;
+    id3 ??= -1;
+
+    //尝试打出两张攻击牌，注意判断是否需要打出第三张
+    await this.playOneCard(id1);
+    await this.playOneCard(id2);
+    await this.playOneCard(id3);
   }
   //打防御牌
   async playDefenseCard() {
--- src/server/utils.ts
+++ src/server/utils.ts
@@ -5,6 +5,7 @@ export async function queryAvatar(userid: string) {
     .get(
       "https://portal-server.eastmoney.com/bd-portal-server/portal/callEmpInfo",
       {
+        timeout: 5 * 1000,
         headers: {
           authToken: "42E0207A33103928EB17A50E4CB035F",
         },
